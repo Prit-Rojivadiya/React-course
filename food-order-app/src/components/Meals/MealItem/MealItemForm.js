828294886234fdcae333a06109f4069a5c4b6e77
Prit Rojivadiya <prit.rojivadiya@marutitech.com>
2023-05-08 11:25:22 +0530

    food order app

---
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,44 @@
+import React, { useRef, useState } from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+
+function MealItemForm(props) {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(amountInputRef);
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (
+      !enteredAmount.trim().length ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          stemp: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+      {!amountIsValid && <p> Please enter a valid amount (1 - 5) </p>}
+    </form>
+  );
+}
+
+export default MealItemForm;
